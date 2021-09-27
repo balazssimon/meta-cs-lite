397f2dc0c3c4a0e462cd84cdeb6ebf08f8287d6a
Balazs Simon <balazssimon@gmail.com>
2021-09-27 15:26:58 +0200

    SourceText ported from Roslyn

---
+++ src/Main/MetaDslx.CodeAnalysis.Common/Diagnostics/ExceptionMessages.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Microsoft.CodeAnalysis
+{
+    internal static class ExceptionMessages
+    {
+        public const string StartMustNotBeNegative = "'start' must not be negative";
+        public const string EndMustNotBeLessThanStart = "'end' must not be less than 'start'";
+
+        public const string ChangesMustBeWithinBoundsOfSourceText = "Changes must be within bounds of SourceText.";
+        public const string ChangesMustNotOverlap = "The changes must not overlap.";
+
+        public const string StreamMustSupportReadAndSeek = "Stream must support read and seek operations.";
+        public const string StreamIsTooLong = "Stream is too long.";
+
+        public const string InvalidHash = "Invalid hash.";
+        public const string UnsupportedHashAlgorithm = "Unsupported hash algorithm.";
+
+        public const string SpanDoesNotIncludeStartOfLine = "The span does not include the start of a line.";
+        public const string SpanDoesNotIncludeEndOfLine = "The span does not include the end of a line.";
+
+    }
+}
