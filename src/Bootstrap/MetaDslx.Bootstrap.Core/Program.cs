57744991d1d8d43184cb355f3b7a997d47f1dd1b
Balazs Simon <balazssimon@gmail.com>
2021-09-27 14:39:59 +0200

    Initial project structure

---
+++ src/Bootstrap/MetaDslx.Bootstrap.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetaDslx.Bootstrap.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
