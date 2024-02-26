1183310e28ef84d50dee02436baa2118308b91fe
Balazs Simon <balazssimon@gmail.com>
2024-02-26 13:48:13 +0100

    Symbols generated

--- src/Main/MetaDslx.CodeAnalysis.Common/Symbols/Custom/TypeSymbolImpl.cs
+++ src/Main/MetaDslx.CodeAnalysis.Common/Symbols/Custom/TypeSymbolImpl.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading;
 using MetaDslx.CodeAnalysis;
 using MetaDslx.CodeAnalysis.Declarations;
+using MetaDslx.CodeAnalysis.PooledObjects;
 using MetaDslx.CodeAnalysis.Symbols;
 
 namespace MetaDslx.CodeAnalysis.Symbols.Impl
@@ -13,6 +14,7 @@ namespace MetaDslx.CodeAnalysis.Symbols.Impl
     using __Model = MetaDslx.Modeling.Model;
     using __IModelObject = MetaDslx.Modeling.IModelObject;
     using __ISymbol = Microsoft.CodeAnalysis.ISymbol;
+    using ITypeSymbol = Microsoft.CodeAnalysis.ITypeSymbol;
 
     public class TypeSymbolImpl : TypeSymbol
     {
@@ -21,9 +23,52 @@ namespace MetaDslx.CodeAnalysis.Symbols.Impl
         {
         }
 
+        protected override ImmutableArray<TypeSymbol> Compute_BaseTypes(DiagnosticBag diagnostics, CancellationToken cancellationToken)
+        {
+            if (IsCSharpSymbol)
+            {
+                var cst = (ITypeSymbol)CSharpSymbol;
+                var baseTypes = ArrayBuilder<TypeSymbol>.GetInstance();
+                if (cst.BaseType is not null)
+                {
+                    var baseType = SymbolFactory.GetSymbol<TypeSymbol>(cst.BaseType, diagnostics, cancellationToken);
+                    if (baseType is not null) baseTypes.Add(baseType);
+                }
+                baseTypes.AddRange(SymbolFactory.GetSymbols<TypeSymbol>(((ITypeSymbol)CSharpSymbol).Interfaces, diagnostics, cancellationToken));
+                return baseTypes.ToImmutableAndFree();
+            }
+            else
+            {
+                return base.Compute_BaseTypes(diagnostics, cancellationToken);
+            }
+        }
+
         protected override ImmutableArray<TypeSymbol> Compute_AllBaseTypes(DiagnosticBag diagnostics, CancellationToken cancellationToken)
         {
-            return BaseTypes; // TODO
+            var allBaseTypes = ArrayBuilder<TypeSymbol>.GetInstance();
+            var stack = ArrayBuilder<TypeSymbol>.GetInstance();
+            stack.Add(this);
+            while (stack.Count > 0)
+            {
+                var type = stack[stack.Count - 1];
+                var added = false;
+                foreach (var baseType in type.BaseTypes)
+                {
+                    if (!stack.Contains(baseType) && !allBaseTypes.Contains(baseType))
+                    {
+                        added = true;
+                        stack.Add(baseType);
+                    }
+                }
+                if (!added)
+                {
+                    if (!allBaseTypes.Contains(type)) allBaseTypes.Add(type);
+                    stack.RemoveAt(stack.Count - 1);
+                }
+            }
+            stack.Free();
+            allBaseTypes.Remove(this);
+            return allBaseTypes.ToImmutableAndFree();
         }
 
         public override bool IsDerivedFrom(TypeSymbol type, TypeEqualityComparer comparison)
